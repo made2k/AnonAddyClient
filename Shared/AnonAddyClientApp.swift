61259dba68f1d1cec5e9168efa70c526771052e5
Zach McGaughey <1799888+made2k@users.noreply.github.com>
2020-12-23 09:31:14 -0700

    Initial Commit

---
+++ Shared/AnonAddyClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnonAddyClientApp.swift
+//  Shared
+//
+//  Created by Zach McGaughey on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct AnonAddyClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
